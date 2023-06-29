bed5bf1b98cc42e6d1e2eaed32a961ce9dafb5e7
toppy007 <33899845+toppy007@users.noreply.github.com>
2023-06-29 10:12:46 +0100

    testing GET request to weather api endpiont

--- src/main.cpp
+++ src/main.cpp
@@ -1,18 +1,66 @@
 #include <Arduino.h>
 
-// put function declarations here:
-int myFunction(int, int);
+#include <ESP8266WiFi.h>
+#include <ESP8266WiFiMulti.h>
+
+#include <ESP8266HTTPClient.h>
+
+#include <WiFiClient.h>
+
+ESP8266WiFiMulti WiFiMulti;
 
 void setup() {
-  // put your setup code here, to run once:
-  int result = myFunction(2, 3);
+
+  Serial.begin(115200);
+  // Serial.setDebugOutput(true);
+
+  Serial.println();
+  Serial.println();
+  Serial.println();
+
+  for (uint8_t t = 4; t > 0; t--) {
+    Serial.printf("[SETUP] WAIT %d...\n", t);
+    Serial.flush();
+    delay(1000);
+  }
+
+  WiFi.mode(WIFI_STA);
+  WiFiMulti.addAP("TOPPY", "top007196");
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-}
+  // wait for WiFi connection
+  if ((WiFiMulti.run() == WL_CONNECTED)) {
 
-// put function definitions here:
-int myFunction(int x, int y) {
-  return x + y;
+    WiFiClient client;
+
+    HTTPClient http;
+
+    Serial.print("[HTTP] begin...\n");
+    // configure traged server and url
+
+    http.begin(client, "http://api.open-meteo.com/v1/forecast?latitude=51.51&longitude=-0.13&hourly=precipitation_probability");
+
+    Serial.print("[HTTP] GET...\n");
+    // start connection and send HTTP header
+    int httpCode = http.GET();
+
+    // httpCode will be negative on error
+    if (httpCode > 0) {
+      // HTTP header has been send and Server response header has been handled
+      Serial.printf("[HTTP] GET... code: %d\n", httpCode);
+
+      // file found at server
+      if (httpCode == HTTP_CODE_OK) {
+        String payload = http.getString();
+        Serial.println(payload);
+      }
+    } else {
+      Serial.printf("[HTTP] GET... failed, error: %s\n", http.errorToString(httpCode).c_str());
+    }
+
+    http.end();
+  }
+
+  delay(10000);
 }
